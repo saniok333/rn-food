1eaeee3f993179f82c72725183334a91f7d8a98e
saniok333 <alex.o.csc@gmail.com>
2022-07-13 15:20:06 +0300

    Added navigation and SearchScreen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,11 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export const SearchScreen = () => {
+  return (
+    <View>
+      <Text>SearchScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
