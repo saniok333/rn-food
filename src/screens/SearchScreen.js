cf45260d2fecb448eac5c233b1191420c212d09b
saniok333 <alex.o.csc@gmail.com>
2022-07-13 23:10:41 +0300

    Request Error handling implementation

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,18 +6,27 @@ import yelp from '../api/yelp';
 export const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: { limit: 50, term, location: 'san jose' },
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await yelp.get('/search', {
+        params: { limit: 50, term, location: 'san jose' },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
   };
 
   return (
     <View>
       <SearchBar onTermChange={setTerm} term={term} onTermSubmit={searchApi} />
-      <Text>We have found {results.length} results</Text>
+      {errorMessage ? (
+        <Text>{errorMessage}</Text>
+      ) : (
+        <Text>We have found {results.length} results</Text>
+      )}
     </View>
   );
 };
