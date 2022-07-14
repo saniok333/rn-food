855623df82f6ea81b25c3d2244edc0184f3696dd
saniok333 <alex.o.csc@gmail.com>
2022-07-14 21:14:25 +0300

    Extracting receving data logic to separate hook

---
+++ src/hooks/useResults.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import yelp from '../api/yelp';
+
+export default useResults = () => {
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get('/search', {
+        params: { limit: 50, term: searchTerm, location: 'san jose' },
+      });
+      setResults(response.data.businesses);
+      setErrorMessage('');
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
+  };
+  useEffect(() => {
+    searchApi('star');
+  }, []);
+  return [results, errorMessage, searchApi];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,26 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { SearchBar } from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 export const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const searchApi = async (searchTerm) => {
-    try {
-      const response = await yelp.get('/search', {
-        params: { limit: 50, term: searchTerm, location: 'san jose' },
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage(err.message);
-    }
-  };
-  useEffect(() => {
-    searchApi('star');
-  }, []);
+  const [results, errorMessage, searchApi] = useResults();
 
   return (
     <View>
@@ -29,11 +14,8 @@ export const SearchScreen = () => {
         term={term}
         onTermSubmit={() => searchApi(term)}
       />
-      {errorMessage ? (
-        <Text>{errorMessage}</Text>
-      ) : (
-        <Text>We have found {results.length} results</Text>
-      )}
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
