61d3f438d210db6f5548d6c56f065818fa9ca0e4
saniok333 <alex.o.csc@gmail.com>
2022-07-15 12:24:01 +0300

    Added additional info to the ResultsDetait component

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,8 +1,25 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 
 export const ResultsDetail = ({ result }) => {
-  const { name } = result;
-  return <Text>{name}</Text>;
+  const { name, image_url, rating, review_count } = result;
+  return (
+    <View>
+      <Image source={{ uri: image_url }} style={styles.imageStyle} />
+      <Text style={styles.nameStyle}>{name}</Text>
+      <Text>
+        {rating} Stars, {review_count} Reviews
+      </Text>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imageStyle: {
+    width: 250,
+    height: 120,
+    borderRadius: 4,
+  },
+  nameStyle: {
+    fontWeight: 'bold',
+  },
+});
